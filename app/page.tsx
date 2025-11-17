fbbe90e73be93df2c68473d1f4a5f98fe9829dd5
joao davi <jd3870894@gmail.com>
2025-11-17 12:04:25 -0300

    modularização dos componentes da home

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,33 @@
-export default async function Home() {
+export default async function Home() {  
+  return (
+    <div className="min-h-screen bg-[#1d2021] text-[#ebdbb2] px-4 py-8">
+      <div className="max-w-3xl mx-auto mb-10">
+        <h2 className="text-2xl font-semibold text-[#fabd2f] mb-4">
+          Posts Recentes
+        </h2>
+        <ListPosts />
+        
+      </div>
+
+        <SearchBox />
+
+      <footer className="text-center text-sm text-[#8ec07c] opacity-80">
+        made by J:D
+      </footer>
+    </div>
+  );
+}
+
+async function ListPosts(){
   const res = await fetch("http://localhost:3000/api/posts", {
     cache: "no-store"
   })
 
   const posts = await res.json()
-  
-  return (
-    <div className="min-h-screen bg-[#1d2021] text-[#ebdbb2] px-4 py-8">
-      <div className="max-w-3xl mx-auto mb-10">
-        <h2 className="text-2xl font-semibold text-[#fabd2f] mb-4">
-          Posts
-        </h2>
+  console.log(posts)
 
-        <div className="bg-[#282828] p-6 rounded-lg border border-[#3c3836]">
+  return(
+    <div className="bg-[#282828] p-6 rounded-lg border border-[#3c3836]">
           {posts.map((post:any) => {
             return(
             <div 
@@ -27,9 +42,12 @@ export default async function Home() {
             )
           })}
         </div>
-      </div>
+  )
+}
 
-      <div className="max-w-3xl mx-auto mb-16">
+function SearchBox(){
+  return(
+    <div className="max-w-3xl mx-auto mb-16">
         <h2 className="text-xl font-semibold text-[#83a598] mb-3">
           Buscar
         </h2>
@@ -40,12 +58,5 @@ export default async function Home() {
           className="w-full px-4 py-2 rounded-md bg-[#282828] text-[#ebdbb2] border border-[#3c3836] focus:outline-none focus:border-[#fe8019]"
         />
       </div>
-
-      <footer className="text-center text-sm text-[#8ec07c] opacity-80">
-        made by J:D
-      </footer>
-    </div>
-  );
+  )
 }
-
-
