2dcaefc80d5ec0eea71650b9f33cf3255ddd705a
joao davi <jd3870894@gmail.com>
2025-11-19 09:33:26 -0300

    criação da função PUT na api

--- app/api/posts/[id]/route.js
+++ app/api/posts/[id]/route.js
@@ -24,3 +24,34 @@ export async function DELETE(req, {params}) {
     return new Response(JSON.stringify({ erro: 'Erro ao deletar o post' }), { status: 500 });
   }
 }
+
+export async function PUT(req, {params}) {
+  const p = await params
+  const id = Number(p.id)
+
+  if(isNaN(id)){
+    return new Response(JSON.stringify({error: "ID inválido"}), {status: 404})
+  }
+
+  if (!title || !content) {
+    return new Response(JSON.stringify({ error: "Título e conteúdo são obrigatórios" }), { status: 400 }); 
+  }
+
+  try {
+    const formData = await req.formData()
+    const title = formData.get('title')
+    const content = formData.get('content')
+
+    const result = await pool.query("UPDATE posts SET title = $1, content = $2 WHERE id = $3", [title,content,id])
+
+    if(result.rowCount === 0){
+      return new Response(JSON.stringify({message: "post não encontrado"}), {status: 404})
+    } 
+
+    return new Response(JSON.stringify({message: "post editado com sucesso"}), {status: 200})
+  } catch (error) {
+    console.log(`erro aqui: ${error}`)
+    return new Response(JSON.stringify({ erro: 'Erro ao editar o post' }), { status: 500 });
+  }
+  
+}
